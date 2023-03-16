96007a02d58296421d8819e4d6886fada3f80d23
Asan Turdukulov <asanturdukulov86@gmail.com>
2023-03-16 11:26:41 +0600

    16.03

---
+++ 15.03/app.js
@@ -0,0 +1,12 @@
+let useName = "Asan";
+alert("Привет! " + useName);
+
+let age = 36;
+
+alert("Мне " + age + " лет!");
+
+let str1 = "САЛАМ, ";
+let str2 = "БИШКЕК!";
+
+alert(str1 + " " + str2);
+
