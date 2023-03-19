a780a8ef884ccb602010b10f8b431b9d7935f4fb
Asan Turdukulov <asanturdukulov86@gmail.com>
2023-03-19 22:21:09 +0600

    17.03

---
+++ 17.03/script.js
@@ -0,0 +1,85 @@
+
+// //true && true = true
+// let first = "true";
+// let second = "true";
+// let third = "true";
+// if(first == second && first == third){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// // true && false = false
+// let first_A = "true";
+// let second_B = "true";
+// let third_C = "false";
+// if(first_A == second_B && first_A == third_C){    
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// //false && true = false
+// let first_a = "true";
+// let second_b = "false";
+// let third_c = "true";
+// if(first_a == second_b && first_a == third_c){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// // false && false = false
+// let first_1 = "false";
+// let second_2 = "true";
+// let third_3 = "false";
+// if(first_1 == second_2 && first_1 == third_3){
+//     console.log("true");
+// }
+// else{
+//     console.log("false")
+// }
+
+/* ******************************************************* */
+
+// true || true = true
+
+let value = 0;
+if(value == 0 || value == 1){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+// false || true = true
+let value1 = 1;
+if(value1 == 0 || value1 == 1){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+// true || false = true
+let value_1 = 1;
+if(value_1 == 1 || value_1 == 1){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+// false || false = false
+let value_a = 0;
+if(value_a == 3 || value_a == 1){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+
+
