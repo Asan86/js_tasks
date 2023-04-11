3d01819455435564a4759916fea0a11ef86b4800
Asan Turdukulov <asanturdukulov86@gmail.com>
2023-04-12 02:41:40 +0600

    10.04

--- 10.04/script.js
+++ 10.04/script.js
@@ -1,28 +1,18 @@
-/* *********************** Получение через цикл (for of) ************************* */
-const form = document.getElementById('my-form');
-const getFormValues1 = (event)=>{
-    event.preventDefault();
-    const values = {};
-    for(let field of form){
-        if(field.name){
-            values[field.name] = field.value;
-        }
-    }
-    console.log(values);
-}
-form.addEventListener('submit', getFormValues1);
+(function () {
+    const forms = document.querySelectorAll(".needs-validation");
+    
+    Array.from(forms).forEach((form) => {
+      form.addEventListener(
+        "submit",
+        (event) => {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  })();
 
-/* *******************Мотод №3 получаем по ключу********************* */
-// const getFormValues2 = (event) => {
-//   event.preventDefault();
-//   const { lname, fname, email, date, adress } = form;
-//   const values = {
-//     lname: lname.value,
-//     fname: fname.value,
-//     email: email.value,
-//     date: date.value,
-//     adress: adress.value,
-//   };
-//   console.log(values);
-// };
-// form.addEventListener("submit", getFormValues2);
